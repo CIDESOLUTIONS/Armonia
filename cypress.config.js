e50216710eaa2dc895602ccddc20987108f1c8a0
CIDE Solutions <meciza@gmail.com>
2025-04-09 12:02:07 -0500

    Migrado Cypress a JavaScript para resolver problemas de compatibilidad

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    // Ajusta `baseUrl` a la URL de tu frontend
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    video: false,
+    chromeWebSecurity: false, // Desactiva la seguridad web de Chrome para pruebas CORS
+  },
+  env: {
+    // URL del backend
+    apiUrl: 'https://localhost:443',
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+    },
+  }
+});
